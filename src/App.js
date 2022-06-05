0baa396c5d415bbdd42022ecab266a9fb5364d36
Antonio Cuellar <lozano.roberto.2018@gmail.com>
2022-06-04 20:35:38 -0500

    se agrega una primera versión rudimentaria de lo que sería un componente de busqueda dentro de un arreglo de datos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { tesisData } from "./tesisData";
+import Highlighter from "react-highlight-words";
+
 
 function App() {
+  const [tesis,setTesis] = useState([]);
+  const [searchResults, setSearchResults] = useState(tesisData);
+  const [descripcion,setDescripcion] = useState("");
+
+  const filterData = (value) => {
+    const lowercasedValue = value.toLowerCase().trim();
+    if (lowercasedValue === '') setTesis(searchResults);
+    else {
+        const filteredData = searchResults.filter((item) => {
+            return Object.keys(item).some((key) => 
+            item[key].toString().toLowerCase().includes(lowercasedValue)
+            );
+        });
+        setTesis(filteredData);
+        
+
+    };
+};
+  
+  const handleChange = (e) =>{
+    setDescripcion(e.target.value);
+    filterData(descripcion);
+  }
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const buscar = formData.get('buscar');
+    filterData(buscar);
+  }
+
+const tesisDisplay = (tesis) =>{
+  return(
+    tesis.map((item)=>(
+      <div key={item.titulo} className='resultados'>
+        <ul>
+          <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.titulo}
+          /> 
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.autor}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.director}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.estado}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.nivel}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.areas}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.estado}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.year.toString()}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.palabrasClave.toString()}
+          /> 
+          </li>
+          <li>
+            
+            <Highlighter
+            highlightClassName="resultados"
+            searchWords={[descripcion]}
+            autoEscape={true}
+            textToHighlight={item.resumen}
+          /> 
+          </li>
+          
+          </ul>
+      </div>
+      
+    ))
+  )
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Filtro de Tesis</h1>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type='text'
+          name='buscar'
+          value={descripcion}
+          placeholder = 'ingrese el parametro de busqueda'
+          onChange = {handleChange}
+          autoComplete ='off'
+        />
+        <button type="submit">Buscar</button>
+      </form>
+      {tesisDisplay(tesis)}
+      
     </div>
   );
 }
